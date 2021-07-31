3ce4560f753064d86a56c82217d33c06e2a42df6
ShareeO <selena74354@yahoo.com>
2021-07-30 23:02:39 -0500

    Contact Page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import AboutMe from './pages/AboutMe';
 import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
       <Header/>
         {/* <AboutMe/> */}
-        <Portfolio/>
+        {/* <Portfolio/> */}
+        <Contact/>
       <Footer/>
     </div>
   );
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+
+function Contact(props) {
+    const[formData, setFormData ] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const handleInputChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;      
+        setFormData({...formData, [name]: value})
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`${formData.name}Thanks for sending a message`)
+
+        setFormData ({
+            name: '',
+            email: '',
+            message: ''
+        })
+    }
 
 
-function Contact (props) {
+
     return (
-        <main>
+    <main>
+        <h1>Contact</h1>
+        <section>
+        <form className="contact-form" onSubmit={handleSubmit}>
+            <div>
+            <label htmlFor="name" className="col-75">
+                Name
+                <input 
+                id="name" 
+                name="name" 
+                type="text" 
+                value={formData.name}
+                onChange={handleInputChange}
+                />
+            </label>
+            </div>
 
-        </main>
-    )
-};
+            <div>
+            <label htmlFor="email" className="col-75">
+                Email
+                <input 
+                id="email" 
+                name="email" 
+                type="text" 
+                value={formData.email} 
+                onChange={handleInputChange}
+                />
+            </label>
+            </div>
+
+            <div>
+            <label htmlFor="message" className="col-75">
+                Message
+                <textarea 
+                id="message" 
+                name="message"                
+                value={formData.message}
+                onChange={handleInputChange}
+                />
+            </label>
+            </div>
+
+            <button type="submit">Submit</button>
+        </form>
+        </section>
+    </main>
+    );
+}
 
 export default Contact;
