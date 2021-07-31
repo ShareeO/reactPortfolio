9f7178aaa3017b2c2ba373655ca3de526c2eec4d
ShareeO <selena74354@yahoo.com>
2021-07-30 21:12:03 -0500

    layout for portfolio

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Portfolio (props) {
+    return (
+        <main>
+            <h1>Portfolio</h1>
+
+            {/* Listing Projects */}
+            <section>
+                
+
+            </section>
+        </main>
+    )
+};
+
+export default Portfolio;
