60cbc5503f0cc196c804852be0d60e629a0f3700
ShareeO <selena74354@yahoo.com>
2021-07-30 21:05:55 -0500

    About Me

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import AboutMe from './pages/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-
+        <AboutMe/>
       <Footer/>
     </div>
   );
---
+++ src/pages/AboutMe.js
@@ -0,0 +1,20 @@
+import React from "react";
+import profile from "../assets/images/MEH.jpeg"
+
+function AboutMe (props) {
+    return (
+        <main>
+            <h1> About Me</h1>
+            <section>
+                <img src={profile}/>
+            </section>
+            <section>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt et harum incidunt odit ea dicta optio rem, ad ipsa libero modi nulla tempore ipsam? Cupiditate, blanditiis, explicabo sed eligendi culpa repudiandae facere ducimus modi non deleniti, numquam dolore mollitia a nostrum. Ullam dolorem dolorum magni animi recusandae obcaecati aut perferendis maxime at facere quod amet, sapiente quis ad ab quas cumque aspernatur assumenda tempora dolores molestias, inventore ipsum! Fuga, ab blanditiis. Laboriosam quod ducimus exercitationem nulla eos officiis aut facere, aliquam totam velit corporis minus commodi iure porro quaerat doloremque obcaecati voluptatum incidunt praesentium minima eligendi! Illum possimus tempora accusamus?</p>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt et harum incidunt odit ea dicta optio rem, ad ipsa libero modi nulla tempore ipsam? Cupiditate, blanditiis, explicabo sed eligendi culpa repudiandae facere ducimus modi non deleniti, numquam dolore mollitia a nostrum. Ullam dolorem dolorum magni animi recusandae obcaecati aut perferendis maxime at facere quod amet, sapiente quis ad ab quas cumque aspernatur assumenda tempora dolores molestias, inventore ipsum! Fuga, ab blanditiis. Laboriosam quod ducimus exercitationem nulla eos officiis aut facere, aliquam totam velit corporis minus commodi iure porro quaerat doloremque obcaecati voluptatum incidunt praesentium minima eligendi! Illum possimus tempora accusamus?</p>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt et harum incidunt odit ea dicta optio rem, ad ipsa libero modi nulla tempore ipsam? Cupiditate, blanditiis, explicabo sed eligendi culpa repudiandae facere ducimus modi non deleniti, numquam dolore mollitia a nostrum. Ullam dolorem dolorum magni animi recusandae obcaecati aut perferendis maxime at facere quod amet, sapiente quis ad ab quas cumque aspernatur assumenda tempora dolores molestias, inventore ipsum! Fuga, ab blanditiis. Laboriosam quod ducimus exercitationem nulla eos officiis aut facere, aliquam totam velit corporis minus commodi iure porro quaerat doloremque obcaecati voluptatum incidunt praesentium minima eligendi! Illum possimus tempora accusamus?</p>
+            </section>
+        </main>
+    )
+};
+
+export default AboutMe;
