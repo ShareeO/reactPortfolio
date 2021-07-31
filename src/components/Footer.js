213c452cabf2c5ba195d53e0636a8f0aa90da3c9
ShareeO <selena74354@yahoo.com>
2021-07-30 20:46:59 -0500

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Footer (props) {
+    return (
+            <footer>
+                <a> LinkedIn </a>
+                <a> GitHub </a>
+                <a> Insta</a>
+            </footer>
+    )
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+function Header (props) {
+    return (
+        <header>
+            <h2>Sharee</h2>
+            <Navigation/>
+        </header>
+
+    )
+};
+
+export default Header;
