87ecb7872954e09f81e6229a421556a532e8827a
ShareeO <selena74354@yahoo.com>
2021-07-30 20:47:12 -0500

    navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="App">
-      <h1> My React Portfolio </h1>
+      <Header/>
+
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Navigation (props) {
+    return (
+        <nav>
+            <ul className="topnav">
+                <li><a> About Me </a></li>
+                <li><a> Contact </a></li>
+                <li><a> Portfolio </a></li>
+                <li><a> Resume </a></li>
+            </ul>
+
+        </nav>
+
+    )
+};
+
+export default Navigation;
